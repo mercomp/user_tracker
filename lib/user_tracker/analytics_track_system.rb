f66224a25313fd69c4879b1173f52958d950a1a5
Aleksander Podlaski <aleksander.podlaski@gmail.com>
2013-10-16 04:13:42 +0200

    analytics-ruby 0.5 changes namespace name

--- lib/user_tracker/analytics_track_system.rb
+++ lib/user_tracker/analytics_track_system.rb
@@ -3,14 +3,14 @@ module UserTracker
     attr_accessor :user_additional_parameters
 
     def initialize(secret_key)
-      ::Analytics.init(secret: secret_key)
+      ::AnalyticsRuby.init(secret: secret_key)
       @user_additional_parameters = -> (user) { { email: user.email} }
     end
 
     def track(event_name, user, parameters)
       identity_user(user)
 
-      ::Analytics.track(
+      ::AnalyticsRuby.track(
         user_id: user.id,
         event: event_name,
         properties: parameters
@@ -18,7 +18,7 @@ module UserTracker
     end
 
     def identity_user(user)
-      ::Analytics.identify(
+      ::AnalyticsRuby.identify(
         user_id: user.id,
         traits: user_additional_parameters.call(user)
       )
--- lib/user_tracker/version.rb
+++ lib/user_tracker/version.rb
@@ -1,3 +1,3 @@
 module UserTracker
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
